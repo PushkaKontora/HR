958d28dd30f59f63ac009af9eafcee89da33d860
Iury Perov <iury.perov@gmail.com>
2022-11-03 20:47:38 +0500

    Optimized resume tests

--- backend/src/tests/v1/integrations/resumes/test_getting_resume.py
+++ backend/src/tests/v1/integrations/resumes/test_getting_resume.py
@@ -1,10 +1,13 @@
+from unittest import mock
+from unittest.mock import PropertyMock
+
 import pytest
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client
 from ninja.responses import Response
 
 from api.models import Competency, Resume, ResumeCompetency, User
-from tests.v1.integrations.conftest import forbidden, get, not_found, datetime_to_string
+from tests.v1.integrations.conftest import datetime_to_string, forbidden, get, not_found
 from tests.v1.integrations.resumes.conftest import RESUME
 
 
@@ -32,61 +35,65 @@ def resume_out(resume: Resume) -> dict:
 
 @pytest.mark.integration
 @pytest.mark.django_db
-def test_get_resume__by_owner(
-    client: Client, resume: Resume, pdf_document: SimpleUploadedFile, user: User, user_token: str
-) -> None:
+def test_get_resume__by_owner(client: Client, resume: Resume, user: User, user_token: str) -> None:
     assert resume.owner == user
-    resume.document = pdf_document
-    resume.save()
 
-    competencies = Competency.objects.bulk_create(Competency(name=str(i)) for i in range(3))
-    ResumeCompetency.objects.bulk_create(ResumeCompetency(resume=resume, competency=comp) for comp in competencies)
+    document = PropertyMock()
+    document.url = "https://lima_dykov.gg"
+    with mock.patch.object(Resume, "document", new_callable=PropertyMock, return_value=document):
+        competencies = Competency.objects.bulk_create(Competency(name=str(i)) for i in range(3))
+        ResumeCompetency.objects.bulk_create(ResumeCompetency(resume=resume, competency=comp) for comp in competencies)
 
-    response = get_one(client, resume.id, user_token)
+        response = get_one(client, resume.id, user_token)
 
-    assert response.status_code == 200
-    assert response.json() == resume_out(resume)
+        assert response.status_code == 200
+        assert response.json() == resume_out(resume)
 
 
 @pytest.mark.integration
 @pytest.mark.django_db
 def test_get_resume__employer_has_access_to_any_resumes(
-    client: Client, resume: Resume, employer: User, employer_token: str, pdf_document: SimpleUploadedFile
+    client: Client, resume: Resume, employer: User, employer_token: str
 ) -> None:
     assert resume.owner != employer
-    resume.document = pdf_document
-    resume.save()
 
-    response = get_one(client, resume.id, employer_token)
+    document = PropertyMock()
+    document.url = "https://lima_dykov.gg"
+    with mock.patch.object(Resume, "document", new_callable=PropertyMock, return_value=document):
+        response = get_one(client, resume.id, employer_token)
 
-    assert response.status_code == 200
-    assert response.json() == resume_out(resume)
-
-
-@pytest.mark.integration
-@pytest.mark.django_db
-def test_get_resume__employer_has_access_to_his_resume(
-    client: Client, employer: User, employer_token: str, pdf_document: SimpleUploadedFile
-) -> None:
-    resume = Resume.objects.create(owner=employer, desired_job="123", document=pdf_document)
-
-    response = get_one(client, resume.id, employer_token)
-
-    assert response.status_code == 200
-    assert response.json() == resume_out(resume)
+        assert response.status_code == 200
+        assert response.json() == resume_out(resume)
 
 
 @pytest.mark.integration
 @pytest.mark.django_db
-def test_get_resume__authenticated_user_cannot_get_anothers_resume(
+def test_get_resume__employer_has_access_to_his_resume(client: Client, employer: User, employer_token: str) -> None:
+    resume = Resume.objects.create(owner=employer, desired_job="123")
+
+    document = PropertyMock()
+    document.url = "https://lima_dykov.gg"
+    with mock.patch.object(Resume, "document", new_callable=PropertyMock, return_value=document):
+        response = get_one(client, resume.id, employer_token)
+
+        assert response.status_code == 200
+        assert response.json() == resume_out(resume)
+
+
+@pytest.mark.integration
+@pytest.mark.django_db
+def test_get_resume__authenticated_user_cannot_get_another_resume(
     client: Client, user_token: str, another_user: User
 ) -> None:
     another_resume = Resume.objects.create(owner=another_user, desired_job="123")
 
-    response = get_one(client, another_resume.id, user_token)
+    document = PropertyMock()
+    document.url = "https://lima_dykov.gg"
+    with mock.patch.object(Resume, "document", new_callable=PropertyMock, return_value=document):
+        response = get_one(client, another_resume.id, user_token)
 
-    assert response.status_code == 403
-    assert response.json() == forbidden()
+        assert response.status_code == 403
+        assert response.json() == forbidden()
 
 
 @pytest.mark.integration
@@ -98,11 +105,3 @@ def test_get_unknown_resume(client: Client, resume: Resume, user_token: str) -> 
 
     assert response.status_code == 404
     assert response.json() == not_found()
-
-
-@pytest.fixture(autouse=True)
-def delete_uploaded_files() -> None:
-    yield
-
-    for resume in Resume.objects.all():
-        resume.document.delete()
