6ea4316433b89f156fab5eeb5fc662fb1641cdd2
Iury Perov <iury.perov@gmail.com>
2022-11-13 14:10:48 +0500

    Added tests for creating a vacancy request

--- backend/src/tests/v1/integrations/resumes/conftest.py
+++ backend/src/tests/v1/integrations/resumes/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.utils.timezone import now
 
 from api.models import Resume, User
@@ -7,11 +6,6 @@ from tests.v1.integrations.conftest import V1
 
 RESUMES = V1 + "/resumes"
 RESUME = RESUMES + "/{resume_id}"
-
-
-@pytest.fixture
-def pdf_document() -> SimpleUploadedFile:
-    return SimpleUploadedFile("test_document.pdf", b"123", content_type="application/pdf")
 
 
 @pytest.fixture
