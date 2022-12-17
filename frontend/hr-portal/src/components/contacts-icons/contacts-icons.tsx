b3100ecb5abc10d6bf8f42333df114b7b6def20a
tania <taniakovesh@gmail.com>
2022-12-17 09:55:31 +0500

    иконки в хедере с ссылками

--- frontend/hr-portal/src/components/contacts-icons/contacts-icons.tsx
+++ frontend/hr-portal/src/components/contacts-icons/contacts-icons.tsx
@@ -1,14 +1,27 @@
-import iconCompany from '../../assets/img/footer/logo-company.svg';
-import logoVK from '../../assets/img/footer/logo-vk.svg';
-import logoWEB from '../../assets/img/footer/logo-web.svg';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import {ReactComponent as LogoVK} from '../../assets/img/footer/logo-vk.svg';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import {ReactComponent as LogoWeb} from '../../assets/img/footer/logo-web.svg';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import {ReactComponent as LogoCompany} from '../../assets/img/footer/logo-company.svg';
+
 import './contacts-icons.scss';
 
 function ContactsIcons() {
   return (
     <div className="contact">
-      <div className="footer-nav"><img src={iconCompany} alt="icon Company"/></div>
-      <div className="footer-nav"><img src={logoVK} alt="logo VK"/></div>
-      <div className="footer-nav"><img src={logoWEB} alt="logo WEB"/></div>
+      <a href="https://www.interesnee.ru/" className="footer-nav">
+        <LogoCompany className="logoCompany"/>
+      </a>
+      <a href="https://vk.com/interesnee_ru" className="footer-nav">
+        <LogoVK className="logoVK"/>
+      </a>
+      <a href="https://www.linkedin.com/company/interesnee-ru/" className="footer-nav">
+        <LogoWeb className="logoLinkin"/>
+      </a>
     </div>
   );
 }
